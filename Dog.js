fd9769899ab0f9d4359d9a6dd18a117d70e69ca8
Andy <andysoutcharith@gmail.com>
2023-08-15 19:37:12 -0500

    Added getDogHtml function to Dog.js, added more styling to index.css like flex utility containers and then added them to html elements, created functionality in index.js that when the cross icon is clicked it renders the next dog object

--- Dog.js
+++ Dog.js
@@ -4,6 +4,15 @@ class Dog {
   constructor(data) {
     Object.assign(this, data);
   }
+
+  getDogHtml() {
+    return `
+      <div class="dog flex-container" style="background-image: url(${this.avatar})">
+        <h1 class="dog-title">${this.name}, ${this.age}</h1>
+        <p>${this.bio}</p>
+      </div>
+    `;
+  }
 }
 
 export default Dog;
--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 import Dog from "./Dog.js";
+import dogs from "./data.js";
+
+let dog = new Dog(dogs[0]);
+
+document.addEventListener("click", function (e) {
+  if (e.target.id === "cross") {
+    dog = new Dog(getNextDog());
+    render();
+  }
+});
+
+const getNextDog = () => dogs.shift();
+
+const render = function () {
+  document.querySelector("main").innerHTML = dog.getDogHtml();
+};
+
+render();
+
+console.log(getNextDog());
